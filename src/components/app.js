b91238fdc4076f19cf432828a05bd502867becf7
Cory Bass <cory.bass.07@gmail.com>
2020-10-21 12:58:05 -0500

    implementing nomatch in react

--- src/components/app.js
+++ src/components/app.js
@@ -8,6 +8,7 @@ import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
 import PortfolioDetail from "./portfolio/portfolio-detail";
+import NoMatch from "./pages/no-match";
 
 export default class App extends Component {
   render() {
@@ -21,10 +22,15 @@ export default class App extends Component {
 
             <Switch>
               <Route exact path="/" component={Home} />
-              <Route path="/about-me" component={About} />
-              <Route path="/contact" component={Contact} />
-              <Route path="/blog" component={Blog} />
-              <Route path="/portfolio/:slug" component={PortfolioDetail} />
+              <Route exact path="/about-me" component={About} />
+              <Route exact path="/contact" component={Contact} />
+              <Route exact path="/blog" component={Blog} />
+              <Route
+                exact
+                path="/portfolio/:slug"
+                component={PortfolioDetail}
+              />
+              <Route component={NoMatch} />
             </Switch>
           </div>
         </Router>
