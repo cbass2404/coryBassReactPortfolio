cb86f3ff14e22122261ff03c252d9eea3e259c58
Cory Bass <cory.bass.07@gmail.com>
2020-10-22 14:30:20 -0500

    Updating Styles with React Event Listeners

--- src/components/portfolio/portfolio-item.js
+++ src/components/portfolio/portfolio-item.js
@@ -1,30 +1,48 @@
-import React from "react";
+import React, { Component } from "react";
 import { Link } from "react-router-dom";
 
-export default function PortfolioItem(props) {
-  // Data that we'll need
-  // - background images: thumb_image_url
-  // - logo: logo_url
-  // - description: description
-  // - id: id
+export default class PortfolioItem extends Component {
+  constructor(props) {
+    super(props);
 
-  const { id, description, thumb_image_url, logo_url } = props.item;
+    this.state = {
+      portfolioItemClass: "",
+    };
+  }
 
-  return (
-    <div className="portfolio-item-wrapper">
+  handleMouseEnter() {
+    this.setState({ portfolioItemClass: "image-blur" });
+  }
+
+  handleMouseLeave() {
+    this.setState({ portfolioItemClass: "" });
+  }
+
+  render() {
+    const { id, description, thumb_image_url, logo_url } = this.props.item;
+
+    return (
       <div
-        className="portfolio-img-background"
-        style={{
-          backgroundImage: "url(" + thumb_image_url + ")",
-        }}
-      />
-      <div className="img-text-wrapper">
-        <div className="logo-wrapper">
-          <img src={logo_url} alt="Logo" />
-        </div>
+        className="portfolio-item-wrapper"
+        onMouseEnter={() => this.handleMouseEnter()}
+        onMouseLeave={() => this.handleMouseLeave()}
+      >
+        <div
+          className={
+            "portfolio-img-background " + this.state.portfolioItemClass
+          }
+          style={{
+            backgroundImage: "url(" + thumb_image_url + ")",
+          }}
+        />
+        <div className="img-text-wrapper">
+          <div className="logo-wrapper">
+            <img src={logo_url} alt="Logo" />
+          </div>
 
-        <div className="subtitle">{description}</div>
+          <div className="subtitle">{description}</div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
