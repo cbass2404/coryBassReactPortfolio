b47d830eaba9081fd18b3b2ca5626fe17923f07e
Cory Bass <cory.bass.07@gmail.com>
2020-10-23 17:04:10 -0500

    How to Show and Hide Links When Logged In

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -2,6 +2,16 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 
 const NavigationContainer = (props) => {
+  const dynamicLink = (route, linkText) => {
+    return (
+      <div className="nav-link-wrapper">
+        <NavLink to="/blog" activeClassName="nav-link-active">
+          Blog
+        </NavLink>
+      </div>
+    );
+  };
+
   return (
     <div className="nav-wrapper">
       <div className="left-side">
@@ -20,11 +30,10 @@ const NavigationContainer = (props) => {
             Contact
           </NavLink>
         </div>
-        <div className="nav-link-wrapper">
-          <NavLink to="/blog" activeClassName="nav-link-active">
-            Blog
-          </NavLink>
-        </div>
+
+        {props.loggedInStatus === "LOGGED_IN"
+          ? dynamicLink("/blog", "BLOG")
+          : null}
       </div>
       <div className="right-side">CORY BASS</div>
     </div>
