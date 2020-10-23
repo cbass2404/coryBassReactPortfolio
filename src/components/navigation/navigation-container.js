8bd0f8eab6060ea6d745f58676cf6f81238110c9
Cory Bass <cory.bass.07@gmail.com>
2020-10-23 16:53:48 -0500

    How to Convert a Class Component into a Functional Component in React

--- src/components/navigation/navigation-container.js
+++ src/components/navigation/navigation-container.js
@@ -1,42 +1,34 @@
-import React, { Component } from "react";
+import React from "react";
 import { NavLink } from "react-router-dom";
 
-class NavigationContainer extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {};
-  }
-
-  render() {
-    return (
-      <div className="nav-wrapper">
-        <div className="left-side">
-          <div className="nav-link-wrapper">
-            <NavLink exact to="/" activeClassName="nav-link-active">
-              Home
-            </NavLink>
-          </div>
-          <div className="nav-link-wrapper">
-            <NavLink to="/about-me" activeClassName="nav-link-active">
-              About
-            </NavLink>
-          </div>
-          <div className="nav-link-wrapper">
-            <NavLink to="/contact" activeClassName="nav-link-active">
-              Contact
-            </NavLink>
-          </div>
-          <div className="nav-link-wrapper">
-            <NavLink to="/blog" activeClassName="nav-link-active">
-              Blog
-            </NavLink>
-          </div>
+const NavigationContainer = (props) => {
+  return (
+    <div className="nav-wrapper">
+      <div className="left-side">
+        <div className="nav-link-wrapper">
+          <NavLink exact to="/" activeClassName="nav-link-active">
+            Home
+          </NavLink>
         </div>
-        <div className="right-side">CORY BASS</div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/about-me" activeClassName="nav-link-active">
+            About
+          </NavLink>
+        </div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/contact" activeClassName="nav-link-active">
+            Contact
+          </NavLink>
+        </div>
+        <div className="nav-link-wrapper">
+          <NavLink to="/blog" activeClassName="nav-link-active">
+            Blog
+          </NavLink>
+        </div>
       </div>
-    );
-  }
-}
+      <div className="right-side">CORY BASS</div>
+    </div>
+  );
+};
 
 export default NavigationContainer;
