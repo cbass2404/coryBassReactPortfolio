2ddbbcd16af602aa918df6f19ab861e0201015d7
Cory Bass <cory.bass.07@gmail.com>
2020-10-26 11:59:53 -0500

    Populating the Portfolio Manager State with API Call Data

--- src/components/pages/portfolio-manager.js
+++ src/components/pages/portfolio-manager.js
@@ -1,10 +1,33 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class PortfolioManager extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      portfolioItems: [],
+    };
   }
+
+  getPortfolioItems() {
+    axios
+      .get("https://corybass.devcamp.space/portfolio/portfolio_items", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        this.setState({
+          portfolioItems: [...res.data.portfolio_items],
+        });
+      })
+      .catch((err) => {
+        console.log("error in get porfolioitems", err);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItems();
+  }
+
   render() {
     return (
       <div className="portfolio-manager-wrapper">
