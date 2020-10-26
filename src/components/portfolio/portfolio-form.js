245fbf86e739e2d75436a3cae1bd0d80d3922cf5
Cory Bass <cory.bass.07@gmail.com>
2020-10-26 16:33:06 -0500

    Guide to Select Dropdowns in React

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -87,13 +87,15 @@ class PortfolioForm extends Component {
               onChange={this.handleChange}
             />
 
-            <input
-              type="text"
+            <select
               name="category"
-              placeholder="Category"
               value={this.state.category}
               onChange={this.handleChange}
-            />
+            >
+              <option value="eCommerce">eCommerce</option>
+              <option value="Scheduling">Scheduling</option>
+              <option value="Enterprise">Enterprise</option>
+            </select>
           </div>
 
           <div>
