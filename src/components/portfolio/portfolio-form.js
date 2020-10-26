9097c7257dc30ed31e8c6b52de32b3850119e389
Cory Bass <cory.bass.07@gmail.com>
2020-10-26 16:26:54 -0500

    Creating Portfolio Items from React Form

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class PortfolioForm extends Component {
   constructor(props) {
@@ -38,7 +39,18 @@ class PortfolioForm extends Component {
   }
 
   handleSubmit(e) {
-    this.buildForm();
+    axios
+      .post(
+        "https://corybass.devcamp.space/portfolio/portfolio_items",
+        this.buildForm(),
+        { withCredentials: true }
+      )
+      .then((res) => {
+        console.log("Response:", res);
+      })
+      .catch((e) => {
+        console.log("portfolio form handleSubmit error", e);
+      });
     e.preventDefault();
   }
 
