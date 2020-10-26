a8517d6a9cdee2119d57c72768c3086889532de7
Cory Bass <cory.bass.07@gmail.com>
2020-10-26 15:11:03 -0500

    Introduction to Building JavaScript FormData Objects

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -19,6 +19,18 @@ class PortfolioForm extends Component {
     this.handleSubmit = this.handleSubmit.bind(this);
   }
 
+  buildForm() {
+    let formData = new FormData();
+
+    formData.append("portfolio_item[name]", this.state.name);
+    formData.append("portfolio_item[description]", this.state.description);
+    formData.append("portfolio_item[url]", this.state.url);
+    formData.append("portfolio_item[category]", this.state.category);
+    formData.append("portfolio_item[position]", this.state.position);
+
+    return formData;
+  }
+
   handleChange(e) {
     this.setState({
       [e.target.name]: e.target.value,
@@ -26,7 +38,7 @@ class PortfolioForm extends Component {
   }
 
   handleSubmit(e) {
-    console.log("Event", e);
+    this.buildForm();
     e.preventDefault();
   }
 
