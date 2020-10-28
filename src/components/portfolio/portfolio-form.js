9ab61aa20c0f508d266916fd4600d2cca6dc29d3
Cory Bass <cory.bass.07@gmail.com>
2020-10-28 16:36:17 -0500

    Guide to Multi Line Ternary Operators in React for Showing and Hiding the Dropzone Components

--- src/components/portfolio/portfolio-form.js
+++ src/components/portfolio/portfolio-form.js
@@ -62,6 +62,9 @@ class PortfolioForm extends Component {
         editMode: true,
         apiUrl: `https://corybass.devcamp.space/portfolio/portfolio_items/${id}`,
         apiAction: "patch",
+        thumb_image: thumb_image_url || "",
+        banner_image: banner_image_url || "",
+        logo: logo_url || "",
       });
     }
   }
@@ -220,14 +223,18 @@ class PortfolioForm extends Component {
         </div>
 
         <div className="image-uploaders three-column">
-          <DropzoneComponent
-            ref={this.thumbRef}
-            config={this.componentConfig()}
-            djsConfig={this.djsConfig()}
-            eventHandlers={this.handleThumbDrop()}
-          >
-            <div className="dz-message">Thumbnail</div>
-          </DropzoneComponent>
+          {this.state.thumb_image && this.state.editMode ? (
+            <img src={this.state.thumb_image} />
+          ) : (
+            <DropzoneComponent
+              ref={this.thumbRef}
+              config={this.componentConfig()}
+              djsConfig={this.djsConfig()}
+              eventHandlers={this.handleThumbDrop()}
+            >
+              <div className="dz-message">Thumbnail</div>
+            </DropzoneComponent>
+          )}
 
           <DropzoneComponent
             ref={this.bannerRef}
