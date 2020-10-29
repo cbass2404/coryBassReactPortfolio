ded4366a14d340ccad9f79af112c95b699b9ef91
Cory Bass <cory.bass.07@gmail.com>
2020-10-29 14:12:00 -0500

    Building the Initial Blog Detail Component

--- src/components/app.js
+++ src/components/app.js
@@ -13,6 +13,7 @@ import Home from "./pages/home";
 import About from "./pages/about";
 import Contact from "./pages/contact";
 import Blog from "./pages/blog";
+import BlogDetail from "./pages/blog-detail";
 import PortfolioManager from "./pages/portfolio-manager";
 import PortfolioDetail from "./portfolio/portfolio-detail";
 import Auth from "./pages/auth";
@@ -118,6 +119,7 @@ export default class App extends Component {
               <Route exact path="/about-me" component={About} />
               <Route exact path="/contact" component={Contact} />
               <Route exact path="/blog" component={Blog} />
+              <Route exact path="/b/:slug" component={BlogDetail} />
               {this.state.loggedInStatus === "LOGGED_IN"
                 ? this.authorizedPages()
                 : null}
---
+++ src/components/pages/blog-detail.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class BlogDetail extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <div>
+        <h1>Blog detail</h1>
+      </div>
+    );
+  }
+}
+
+export default BlogDetail;
