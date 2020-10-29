2bc06fb2db689bc72489a5fa511f057093d9a2a7
Cory Bass <cory.bass.07@gmail.com>
2020-10-29 12:49:44 -0500

    Calling the Blog API and Storing the Data in State

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,11 +1,35 @@
 import React, { Component } from "react";
+import axios from "axios";
 import { Link } from "react-router-dom";
 
 class Blog extends Component {
   constructor(props) {
     super(props);
 
-    this.state = {};
+    this.state = {
+      blogItems: [],
+    };
+
+    this.getBlogItems = this.getBlogItems.bind(this);
+  }
+
+  getBlogItems() {
+    axios
+      .get("https://corybass.devcamp.space/portfolio/portfolio_blogs", {
+        withCredentials: true,
+      })
+      .then((res) => {
+        this.setState({
+          blogItems: res.data.portfolio_blogs,
+        });
+      })
+      .catch((err) => {
+        console.log("getBlogItems ERROR:", err);
+      });
+  }
+
+  componentWillMount() {
+    this.getBlogItems();
   }
 
   render() {
