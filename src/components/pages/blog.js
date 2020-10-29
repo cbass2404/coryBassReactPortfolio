e238822dd1be4c5dac33f483f915bbd536649f51
Cory Bass <cory.bass.07@gmail.com>
2020-10-29 13:10:17 -0500

    lunch

--- src/components/pages/blog.js
+++ src/components/pages/blog.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
-import axios from "axios";
 import { Link } from "react-router-dom";
+import axios from "axios";
+
+import BlogItem from "../blog/blog-item";
 
 class Blog extends Component {
   constructor(props) {
@@ -34,7 +36,7 @@ class Blog extends Component {
 
   render() {
     const blogRecords = this.state.blogItems.map((blogItem) => {
-      return <h1>{blogItem.title}</h1>;
+      return <BlogItem key={blogItem.id} blogItem={blogItem} />;
     });
     return <div>{blogRecords}</div>;
   }
