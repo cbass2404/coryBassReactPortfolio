38aaa2afd0ed658eee7dd77699e2c323616cff1b
Cory Bass <cory.bass.07@gmail.com>
2020-11-03 14:58:23 -0600

    Revisiting Render Props and Passing the Logged In Status to Child Components

--- src/components/app.js
+++ src/components/app.js
@@ -120,7 +120,15 @@ export default class App extends Component {
               />
               <Route exact path="/about-me" component={About} />
               <Route exact path="/contact" component={Contact} />
-              <Route exact path="/blog" component={Blog} />
+
+              <Route
+                exact
+                path="/blog"
+                render={(props) => (
+                  <Blog {...props} loggedInStatus={this.state.loggedInStatus} />
+                )}
+              />
+
               <Route exact path="/b/:slug" component={BlogDetail} />
               {this.state.loggedInStatus === "LOGGED_IN"
                 ? this.authorizedPages()
