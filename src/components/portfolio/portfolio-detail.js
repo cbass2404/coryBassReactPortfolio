074ea9007e56fe031e9b96c4db9643a4815d5ea1
Cory Bass <cory.bass.07@gmail.com>
2020-11-05 14:41:06 -0600

    Initial Build Out of the Portfolio Detail Component

--- src/components/portfolio/portfolio-detail.js
+++ src/components/portfolio/portfolio-detail.js
@@ -1,11 +1,55 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const PortfolioDetail = (props) => {
-  return (
-    <div>
-      <h2>Portfolio Detail for {props.match.params.slug}</h2>
-    </div>
-  );
-};
+class PortfolioDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      portfolioItem: {},
+    };
+
+    this.getPortfolioItem = this.getPortfolioItem.bind(this);
+  }
+
+  getPortfolioItem() {
+    axios
+      .get(
+        `https://corybass.devcamp.space/portfolio/portfolio_items/${this.props.match.params.slug}`,
+        { withCredentials: true }
+      )
+      .then((res) => {
+        this.setState({
+          portfolioItem: res.data.portfolio_item,
+        });
+      })
+      .catch((err) => {
+        console.log("portfolio-detail getPortfolioItem", err);
+      });
+  }
+
+  componentDidMount() {
+    this.getPortfolioItem();
+  }
+
+  render() {
+    const {
+      id,
+      name,
+      description,
+      url,
+      category,
+      thumb_image_url,
+      banner_image_url,
+      logo_url,
+    } = this.state.portfolioItem;
+
+    return (
+      <div>
+        <h2>Portfolio Detail for {this.props.match.params.slug}</h2>
+      </div>
+    );
+  }
+}
 
 export default PortfolioDetail;
